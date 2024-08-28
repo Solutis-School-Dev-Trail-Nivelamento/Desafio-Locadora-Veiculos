b924bb40a716e9cded2cbfba4d6af09d409f0141
Filipe Campos <filipe_sobre1@hotmail.com>
2024-08-28 14:59:22 -0300

    Criação das classes Fabricante, Modelo Carro, e seus Repository's.Precisa de atualização

---
+++ src/main/java/br/edu/solutis/dev/trail/locadora/repository/FabricanteRepository.java
@@ -0,0 +1,10 @@
+package br.edu.solutis.dev.trail.locadora.repository;
+
+import br.edu.solutis.dev.trail.locadora.model.entity.Fabricante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FabricanteRepository extends JpaRepository<Fabricante, Long> {
+    boolean existsBy //para não ser duplicado atualizar mais tarde
+}
---
+++ src/main/java/br/edu/solutis/dev/trail/locadora/repository/ModeloCarroRepository.java
@@ -0,0 +1,12 @@
+package br.edu.solutis.dev.trail.locadora.repository;
+
+
+import br.edu.solutis.dev.trail.locadora.model.entity.ModeloCarro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModeloCarroRepository extends JpaRepository<ModeloCarro, Long> {
+    boolean existsBy // para não ser duplicado atualizar mais tarde
+}
+
