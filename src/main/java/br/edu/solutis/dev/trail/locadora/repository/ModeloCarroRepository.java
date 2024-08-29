5dfb794dc8e150596c190d7b6841ea24b373b5f6
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-08-28 21:12:45 -0300

    updated: modelo carro repository

--- src/main/java/br/edu/solutis/dev/trail/locadora/repository/ModeloCarroRepository.java
+++ src/main/java/br/edu/solutis/dev/trail/locadora/repository/ModeloCarroRepository.java
@@ -1,17 +1,12 @@
 package br.edu.solutis.dev.trail.locadora.repository;
 
-
-import br.edu.solutis.dev.trail.locadora.model.entity.Carro;
+import br.edu.solutis.dev.trail.locadora.model.entity.ModeloCarro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
-public interface ModeloCarroRepository extends JpaRepository<Carro, Long> {
-   // boolean existsBy // para não ser duplicado atualizar mais tarde
-=======
 public interface ModeloCarroRepository extends JpaRepository<ModeloCarro, Long> {
    boolean existsByDescricao (String descricao);
->>>>>>> e5d40ca84ecfe6020fc4f0d14a5ea604b2fe03c8
+
 }
 
