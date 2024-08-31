f303bba63f8a0996df8509946e4ab51ed1448387
beatrizx2 <beaazevedo14@gmail.com>
2024-08-31 19:30:03 -0300

    Imports necessarios para aluguel repository

--- src/main/java/br/edu/solutis/dev/trail/locadora/repository/AluguelRepository.java
+++ src/main/java/br/edu/solutis/dev/trail/locadora/repository/AluguelRepository.java
@@ -2,6 +2,8 @@ package br.edu.solutis.dev.trail.locadora.repository;
 
 import br.edu.solutis.dev.trail.locadora.model.entity.Aluguel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
