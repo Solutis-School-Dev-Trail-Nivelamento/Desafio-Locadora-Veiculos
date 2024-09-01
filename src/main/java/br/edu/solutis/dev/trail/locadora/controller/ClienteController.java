8285c495c434aa1b99c58195f46fc9f44592bf2f
Heloisa Felizardo <helocmp@yahoo.com.br>
2024-09-01 10:11:05 -0300

    updated: edpoints cliente

--- src/main/java/br/edu/solutis/dev/trail/locadora/controller/ClienteController.java
+++ src/main/java/br/edu/solutis/dev/trail/locadora/controller/ClienteController.java
@@ -1,6 +1,8 @@
 package br.edu.solutis.dev.trail.locadora.controller;
 
+import br.edu.solutis.dev.trail.locadora.model.entity.Aluguel;
 import br.edu.solutis.dev.trail.locadora.model.entity.Cliente;
+import br.edu.solutis.dev.trail.locadora.service.AluguelService;
 import br.edu.solutis.dev.trail.locadora.service.ClienteService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -12,43 +14,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cliente")
+@RequestMapping("/")
 @Tag(name = "Cliente")
 public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
 
-    @PostMapping("/cadastro")
+    @Autowired
+    private AluguelService aluguelService;
+
+    @PostMapping("/cliente")
     @Operation(summary = "Cadastra um novo cliente", description = "Cadastra um novo cliente e retorna os dados criados")
     public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
         Cliente clienteCriado = clienteService.salvarCliente(cliente);
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteCriado);
     }
 
-    @GetMapping("/obterTodos")
+    @GetMapping("/clientes")
     @Operation(summary = "Obtém todos clientes", description = "Retorna a informação com todos clientes cadastrados")
     public ResponseEntity<List<Cliente>> obterTodosClientes() {
         List<Cliente> clientes = clienteService.obterTodos();
         return new ResponseEntity<>(clientes, HttpStatus.OK);
     }
 
-    @GetMapping("/obterCliente/{id}")
-    @Operation(summary = "Obtém cliente por ID", description = "Retorna cliente pelo ID")
+    @GetMapping("/cliente/{id}")
+    @Operation(summary = "Obtém um cliente pelo ID", description = "Retorna cliente pelo ID")
     public ResponseEntity<Cliente> obterCliente(@PathVariable Long id) {
         Cliente cliente = clienteService.obterPorId(id);
         return ResponseEntity.ok(cliente);
     }
 
-    @PutMapping("/atualizar/{id}")
-    @Operation(summary = "Atualiza cliente", description = "Atualiza as informações de um cliente")
+    @PutMapping("/cliente/{id}")
+    @Operation(summary = "Atualiza um cliente pelo ID", description = "Atualiza as informações de um cliente")
     public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
         Cliente clienteAtualizado = clienteService.atualizarCliente(id, cliente);
         return ResponseEntity.ok(clienteAtualizado);
     }
 
-    @DeleteMapping("/delete/{id}")
-    @Operation(summary = "Deleta cliente", description = "Deleta cliente pelo ID")
+    @DeleteMapping("/cliente/{id}")
+    @Operation(summary = "Remove um cliente pelo ID", description = "Deleta cliente pelo ID")
     public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
         clienteService.excluirCliente(id);
         return ResponseEntity.noContent().build();
@@ -57,7 +62,7 @@ public class ClienteController {
     /*@GetMapping("/cliente/{id}/alugueis")
     @Operation(summary = "Exibe os aluguéis do usuário", description = "Exibe ao usuário todos os aluguéis relacionados ao seu id")
     public ResponseEntity<List<Aluguel>> alugueisConfirmados(@PathVariable Long id) {
-        List<Aluguel> alugueis = clienteService.alugueisConfirmados(id);
+        List<Aluguel> alugueis = clienteService.(id);
         return ResponseEntity.ok(alugueis);
     }*/
 }
