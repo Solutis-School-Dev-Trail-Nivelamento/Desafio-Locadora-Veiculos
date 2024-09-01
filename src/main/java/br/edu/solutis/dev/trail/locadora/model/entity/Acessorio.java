703ed3ffc4474243bc85f5b420f38edbbdae1ac8
VininLeao <vininfleao@hotmail.com>
2024-09-01 17:04:28 -0300

    fix: resolvendo conflitos entre tabelas acessorio e carro

--- src/main/java/br/edu/solutis/dev/trail/locadora/model/entity/Acessorio.java
+++ src/main/java/br/edu/solutis/dev/trail/locadora/model/entity/Acessorio.java
@@ -4,16 +4,20 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.ToString;
 
+import java.util.List;
+
 @Data
 @ToString
 @Entity
-@Table(name= "tb_carro_acessorio")
+@Table(name= "tb_acessorio")
 public class Acessorio {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(length = 100, nullable = false, unique = true)
     private String descricao;
+
+    @ManyToMany(mappedBy = "acessorios")
+    private List<Carro> carros;
 }
